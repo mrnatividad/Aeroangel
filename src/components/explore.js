a960cd192ac712b7d4fc94e31961336a1ca7dc59
Renee <renee.natividad718@gmail.com>
2024-10-18 04:37:28 +0800

    final na talaga to

--- src/components/explore.js
+++ src/components/explore.js
@@ -5,13 +5,13 @@ const Explore = () => {
         <div className="explore">
             <div className="title-explore inknut-antiqua-normal">Types of Aircraft</div>
             <div className="explore-container">
-                <img src = "/EXPLORE/LIGHT.jpg" className="explore-pic" alt= 'p1' />
-                <img src = "/EXPLORE/LOW.jpg" className="explore-pic" alt= 'p2' />
-                <img src = "/EXPLORE/SAILPLANE.jpg" className="explore-pic" alt= 'p3' />
-                <img src = "/EXPLORE/COMMERCIAL.jpg" className="explore-pic" alt= 'p4' />
-                <img src = "/EXPLORE/AGRICULTURAL.jpg" className="explore-pic" alt= 'p5' />
-                <img src = "/EXPLORE/ATTACK.jpg" className="explore-pic" alt= 'p6' />
-                <img src = "/EXPLORE/UAV.jpg" className="explore-pic" alt= 'p7' />
+                <img src = "/EXPLORE/LIGHT.JPG" className="explore-pic" alt= 'p1' />
+                <img src = "/EXPLORE/LOW.JPG" className="explore-pic" alt= 'p2' />
+                <img src = "/EXPLORE/SAILPLANE.JPG" className="explore-pic" alt= 'p3' />
+                <img src = "/EXPLORE/COMMERCIAL.JPG" className="explore-pic" alt= 'p4' />
+                <img src = "/EXPLORE/AGRICULTURAL.JPG" className="explore-pic" alt= 'p5' />
+                <img src = "/EXPLORE/ATTACK.JPG" className="explore-pic" alt= 'p6' />
+                <img src = "/EXPLORE/UAV.JPG" className="explore-pic" alt= 'p7' />
             </div>
         </div>
     )
